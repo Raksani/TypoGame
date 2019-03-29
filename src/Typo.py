172a9e4e196b7e8e4d85e4aa8a8a55f94fbdcc62
Raksani <raksani.k@ku.th>
2019-03-29 14:32:51 +0700

    add words class and function on key press also with setup in Typo

--- src/Typo.py
+++ src/Typo.py
@@ -1,4 +1,9 @@
 import arcade
+import enum
+class GameStates(enum.Enum):
+    GAME_OVER = 0
+    RUNNING = 1
+
 class Typo(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height, title="Typo Game")
@@ -8,7 +13,25 @@ class Typo(arcade.Window):
         # self.high_score = int()
         self.score = int()
         self.lives = int()
-        self.state = None
         self.focus_word = None  #The word that is currently being typed.
         self.word_list = set()
-        self.star_list = set()
+
+    def setup(self):
+        self.score = 0
+        self.lives = 3 # edit the lives maybe 4
+        self.state = GameStates.RUNNING
+        self.focus_word = None #The word that is currently being typed.
+        self.word_list = set()
+
+
+    def draw_game(self):
+        for word in self.word_list:
+            word.draw()
+
+        arcade.draw_text(f"Score : {self.score}", 15, 15, arcade.color.WHITE, 15)
+        arcade.draw_text(f"Lives : {self.lives}", self.screen_width - 15, 15, arcade.color.WHITE, 14, align="right",
+                         anchor_x="right", anchor_y="baseline")
+
+    def on_key_press(self, symbol: int, modifiers: int):
+
+
---
+++ src/Words.py
@@ -0,0 +1,27 @@
+import arcade
+# from Typo import Typo
+WORD_LIST = [
+    "software", "computer", "java", "python", "arcade", "game", "laptop" , "www",
+    "https", "glasses"
+]
+class Words:
+    def __init__(self, words, row, screen_width, screen_height):
+        self.words = words
+        self.row = row
+        self.x = screen_width
+        self.y = screen_height
+        self.in_focus = False
+        self.color = None
+
+    def change_color(self):
+        if self.in_focus:
+            self.color = arcade.color.RED
+        else:
+            self.color = arcade.color.WHITE
+
+    def draw(self):
+        arcade.draw_text(self.word, self.x, self.y,self.color,14)
+
+    # decrease the word 1 by 1 by typing.
+    def encounter(self):
+        self.words = self.words[1:]
