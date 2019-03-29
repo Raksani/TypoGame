c06509921c19f1a967020a482ebc6326a5fdd021
Raksani <raksani.k@ku.th>
2019-03-29 13:33:05 +0700

    add constants

--- src/Typo.py
+++ src/Typo.py
@@ -2,4 +2,13 @@ import arcade
 class Typo(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height, title="Typo Game")
-        
+        arcade.set_background_color((5, 2, 27))
+        self.screen_width = width
+        self.screen_height = height
+        # self.high_score = int()
+        self.score = int()
+        self.lives = int()
+        self.state = None
+        self.focus_word = None  #The word that is currently being typed.
+        self.word_list = set()
+        self.star_list = set()
